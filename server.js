8dd919071c477a454da9b050c34e30d17635dc93
AnneJ <73593442+annevjenkins42@users.noreply.github.com>
2023-09-07 10:50:53 +0100

    Update server.js

--- server.js
+++ server.js
@@ -14,8 +14,38 @@ app.use("/images", express.static(path.join(__dirname, 'images')));
 
 const server = http.createServer((req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.writeHead(200, { 'content-type': 'text/html' })
-  fs.createReadStream('collections_demo.html').pipe(res)
+  if(req.url === "/"){
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	fs.createReadStream('index.html').pipe(res)
+  }
+  else if(/^\/[a-zA-Z0-9\/-]*.js$/.test(req.url.toString())){
+	sendFileContent(res, req.url.toString().substring(1), "text/javascript");
+  }
+  else if(/^\/[a-zA-Z0-9\/-]*.css$/.test(req.url.toString())){
+	sendFileContent(res, req.url.toString().substring(1), "text/css");
+  }
+  else if(/^\/[a-zA-Z0-9\/-]*.png$/.test(req.url.toString())){
+	sendFileContent(res, req.url.toString().substring(1), "image/text");
+  }
+  else{
+	console.log("Requested URL is: " + req.url);
+	res.end();
+  }
+	
 })
 
 server.listen(process.env.PORT || 3000)
+
+function sendFileContent(response, fileName, contentType){
+	fs.readFile(fileName, function(err, data){
+		if(err){
+			response.writeHead(404);
+			response.write("Not Found!");
+		}
+		else{
+			response.writeHead(200, {'Content-Type': contentType});
+			response.write(data);
+		}
+		response.end();
+	});
+}
